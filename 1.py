e81eeab3be98b4e03222d8449ab6a191945d2704
Z001 <uplink03@gmail.com>
2014-05-01 22:13:58 +0400

    Simple interface
    Now user being propted for amount of pushes needed to be downloaded

--- 1.py
+++ 1.py
@@ -3,13 +3,14 @@ import string
 import urllib
 
 counter1 = 0
+download = int(raw_input("How many puushes you like to download?: "))
 char_set = string.digits
 char_set1 = string.ascii_lowercase
 char_set2 = string.ascii_uppercase
 char_set3 = string.ascii_uppercase + string.digits
-while counter1 <=300:
+while counter1 <= download:
 	a = ''.join(random.sample(char_set*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set2*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set3*1, 1))
 	b = 'http://puu.sh/' + a + '.png'
 	counter1 += 1
-	print b
+	print b , 'downloaded'
 	urllib.urlretrieve(b, a + '.png')
