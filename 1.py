9f2088a8b26df3a273dce592d3363c9af633a9da
Z001 <uplink03@gmail.com>
2014-05-01 14:26:46 +0400

    major improvements
    png instead of jpg, improved charsets. valid rate up to 70% more.

--- 1.py
+++ 1.py
@@ -1,18 +1,15 @@
 import random
 import string
 import urllib
+
 counter1 = 0
-char_set = string.ascii_uppercase + string.digits + string.ascii_lowercase
-while counter1 <=200:
-	a = ''.join(random.sample(char_set*5, 5))
-	b = 'http://puu.sh/' + a + '.jpg'
+char_set = string.digits
+char_set1 = string.ascii_lowercase
+char_set2 = string.ascii_uppercase
+char_set3 = string.ascii_uppercase + string.digits
+while counter1 <=300:
+	a = ''.join(random.sample(char_set*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set2*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set3*1, 1))
+	b = 'http://puu.sh/' + a + '.png'
 	counter1 += 1
 	print b
-	urllib.urlretrieve(b, a + '.jpg')
-
-	
-		
-		
-
-
-	
+	urllib.urlretrieve(b, a + '.png')
