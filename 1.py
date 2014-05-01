49a27cfa7595f0306e1ebdac4754151f548ba964
Z001 <uplink03@gmail.com>
2014-05-02 03:05:45 +0400

    attempts = downloaded pushes
    fixed counters

--- 1.py
+++ 1.py
@@ -3,20 +3,25 @@ import string
 import urllib
 from urllib import urlopen
 
-counter1 = 0
+
+
 download = int(raw_input("How many attempts you like to try?: "))
+counter1 = 0
 char_set = string.digits
 char_set1 = string.ascii_lowercase
 char_set2 = string.ascii_uppercase
 char_set3 = string.ascii_uppercase + string.digits
 
-for i in range(download):
+
+while counter1 <= download:
 	rand = ''.join(random.sample(char_set*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set2*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set3*1, 1)) + '.png'
-	fname = 'http://puu.sh/' + rand 
-	content = urlopen('http://puu.sh/' + rand).read()
-	length = len(content)
-	if length > 100:
+	fname = 'http://puu.sh/' + rand
+	rcode = urlopen('http://puu.sh/' + rand).getcode()
+	if rcode == 200:
+		content = urlopen('http://puu.sh/' + rand).read()
+		length = len(content)
 		fo = open("{0}".format(rand), "wb")
 		fo.write(content);
 		fo.close()
-		print fname , 'downloaded' , length, "bytes"
+		counter1 += 1
+		print fname , rcode, 'downloaded' , length, "bytes"
