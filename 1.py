bd9affcb380b756185c958573ea18e4815ec0dc8
John Di <Z001@live.ru>
2014-05-02 23:21:39 +0400

    gui added
    tk gui added. yay!

--- 1.py
+++ 1.py
@@ -1,27 +1,93 @@
+from Tkinter import *
+
 import random
+
 import string
+
 import urllib
+
 from urllib import urlopen
 
 
 
-download = int(raw_input("How many attempts you like to try?: "))
-counter1 = 0
-char_set = string.digits
-char_set1 = string.ascii_lowercase
-char_set2 = string.ascii_uppercase
-char_set3 = string.ascii_uppercase + string.digits
+master = Tk()
 
 
-while counter1 <= download:
-	rand = ''.join(random.sample(char_set*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set2*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set3*1, 1)) + '.png'
-	fname = 'http://puu.sh/' + rand
-	rcode = urlopen('http://puu.sh/' + rand).getcode()
-	if rcode == 200:
-		content = urlopen('http://puu.sh/' + rand).read()
-		length = len(content)
-		fo = open("{0}".format(rand), "wb")
-		fo.write(content);
-		fo.close()
-		counter1 += 1
-		print fname , rcode, 'downloaded' , length, "bytes"
+
+w = Label(master, text="How many puushes would you like to download?")
+
+w.pack()
+
+
+
+separator = Frame(height=2, bd=1, relief=SUNKEN)
+
+separator.pack(fill=X, padx=5, pady=5)
+
+
+
+e = Entry(master)
+
+e.pack()
+
+
+
+e.focus_set()
+
+
+
+def callback():
+
+	download = int(e.get())
+
+	counter1 = 1
+
+	char_set = string.digits
+
+	char_set1 = string.ascii_lowercase
+
+	char_set2 = string.ascii_uppercase
+
+	char_set3 = string.ascii_uppercase + string.digits
+
+	while counter1 <= download:
+
+		print counter1
+
+		rand = ''.join(random.sample(char_set*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set2*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set3*1, 1)) + '.png'
+
+		fname = 'http://puu.sh/' + rand
+
+		rcode = urlopen('http://puu.sh/' + rand).getcode()
+
+		if rcode == 200:
+
+			content = urlopen('http://puu.sh/' + rand).read()
+
+			length = len(content)
+
+			fo = open("{0}".format(rand), "wb")
+
+			fo.write(content);
+
+			fo.close()
+
+			counter1 += 1
+
+			print fname , rcode, 'downloaded' , length, "bytes"
+
+    			
+
+separator = Frame(height=2, bd=1, relief=SUNKEN)
+
+separator.pack(fill=X, padx=5, pady=5)
+
+
+
+b = Button(master, text="Grab it!", width=10, command=callback)
+
+b.pack()
+
+
+
+mainloop()
