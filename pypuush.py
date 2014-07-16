c34a1f85158f0364408d62ad1261b59d7fd72a7c
Z001 <uplink03@gmail.com>
2014-07-16 17:52:02 +0400

    fix
    fix

---
+++ pypuush.py
@@ -0,0 +1,35 @@
+import random
+import string
+import urllib
+from urllib import urlopen
+import datetime
+import time
+
+download = int(raw_input("How many attempts you like to try?: "))
+counter1 = 0
+char_set = string.digits
+char_set1 = string.ascii_lowercase
+char_set2 = string.ascii_uppercase
+char_set3 = string.ascii_uppercase + string.digits
+
+while counter1 < download:
+	rand = ''.join(random.sample(char_set*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set2*1, 1)) + ''.join(random.sample(char_set1*1, 1)) + ''.join(random.sample(char_set3*1, 1)) + '.png'
+	fname = 'http://puu.sh/' + rand
+	rcode = urlopen('http://puu.sh/' + rand).getcode()
+	t1 = time.time()
+	st = datetime.datetime.fromtimestamp(t1).strftime('%Y-%m-%d %H:%M:%S')
+	if rcode == 200:
+		content = urlopen('http://puu.sh/' + rand).read()
+		length = len(content)
+		fo = open("{0}".format(rand), "wb")
+		fo.write(content);
+		fo.close()
+		counter1 += 1
+
+		print st, " |", fname, " |", rcode, "Ok |", 'Downloaded:' , length, "bytes."
+	elif rcode == 403:
+		print st, " |", fname, " |", rcode, "Forbidden |"
+	elif rcode == 404:
+		print st, " |", fname, " |", rcode, "Not found |"
+	elif rcode == 300:
+		print "Otsosi u traktorista"
